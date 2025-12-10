e8b5682c69ac28ce65d71f114f0ad7ec17d6d31e
elputometal <pdsebastian92@gmail.com>
2025-12-10 20:58:51 +0100

    asteroids-CH3L4

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 from logger import log_state
 import pygame
+import player
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 
 def main():
+    dt, elapsedTime = 0, 0
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}\n")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    pygame.time.Clock()
 
+    playerInstance = player.Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+    
     while True:
         log_state()
 
@@ -17,8 +22,15 @@ def main():
             if event.type == pygame.QUIT:
                 return
 
+        elapsedTime = pygame.time.Clock().tick(60)
+        dt = elapsedTime / 1000
+        #print(f"DeltaTime: {dt}")
+
         screen.fill("black")
+        playerInstance.update(dt)
+        playerInstance.draw(screen)
         pygame.display.flip()
+
 
 if __name__ == "__main__":
     main()
